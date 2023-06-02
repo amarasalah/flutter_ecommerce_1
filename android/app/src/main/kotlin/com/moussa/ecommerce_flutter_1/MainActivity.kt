ff3f6293944904b4a94d6a349b1ca5483188818e
amarasalah <amarasalah@outlook.com>
2023-06-02 13:08:13 +0100

    flutter project ecommerce

---
+++ android/app/src/main/kotlin/com/moussa/ecommerce_flutter_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.moussa.ecommerce_flutter_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ecommerce_flutter_1/view/screens/onboarding.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const OnBoarding(),
+    );
+  }
+}
